7bf674c5f3ed59bca527a78eb2ccb4fd32714c25
VictorMMosqueraG <victormmosquerag@gmail.com>
2026-02-18 10:33:52 -0500

    feature(createInvoice): feature was created

---
+++ application/Features/Invoices/CreateInvoice/Mappers/CreateInvoiceMapper.cs
@@ -0,0 +1,22 @@
+using Application.Features.Invoices.CreateInvoice.Dtos;
+using AutoMapper;
+using Core.Entities;
+using Core.Constants;
+
+namespace Application.Features.Invoices.CreateInvoice.Mappers;
+
+public class CreateInvoiceMapper : Profile
+{
+    public CreateInvoiceMapper()
+    {
+        CreateMap<CreateInvoiceRequestDto, Invoice>()
+            .ForMember(dest => dest.ClientId, opt => opt.MapFrom(src => src.ClientId))
+            .ForMember(dest => dest.InvoiceNumber, opt => opt.MapFrom(src => src.InvoiceNumber))
+            .ForMember(dest => dest.Amount, opt => opt.MapFrom(src => src.Amount))
+            .ForMember(dest => dest.DueDate, opt => opt.MapFrom(src => src.DueDate))
+            .ForMember(dest => dest.Description, opt => opt.MapFrom(src => src.Description))
+            .ForMember(dest => dest.Status, opt => opt.MapFrom(src => InvoiceStatus.Pending))
+            .ForMember(dest => dest.CreatedAt, opt => opt.MapFrom(src => DateTime.UtcNow))
+            .ForMember(dest => dest.UpdatedAt, opt => opt.MapFrom(src => DateTime.UtcNow));
+    }
+}
